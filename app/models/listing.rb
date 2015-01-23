e52d5346d70af7e3ffac77751559926b566d96ee
Tim Billington <tsbillington4@gmail.com>
2015-01-23 17:59:36 -0500

    associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,15 +1,18 @@
 class Listing < ActiveRecord::Base
 	if Rails.env.development?
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.gif"
+		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	else
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.gif",
 		   					:storage => :dropbox,
 	   						:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 	   						:path => ":style/:id_:filename"
-		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	end
 
 	validates :name, :description, :price, presence: true
 	validates :price, numericality: { greater_than: 0 }
 	validates_attachment_presence :image
+
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150123181127) do
+ActiveRecord::Schema.define(version: 20150123213356) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150123181127) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "models", force: :cascade do |t|
